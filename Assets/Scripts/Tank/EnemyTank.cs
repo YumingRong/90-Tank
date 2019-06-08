cab2fa1dc41a74bbb981221ef15b5626c428181b
Rong Yuming <rym_shunv@msn.com>
2019-06-09 00:11:15 +0800

    Re add Enemytank because I mistakenly removed it.

---
+++ Assets/Scripts/Tank/EnemyTank.cs
@@ -0,0 +1,147 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Tilemaps;
+
+public class EnemyTank : Tank
+{
+    private float directionChangeInteval;
+    private float directionChangeTimer;
+
+    private int[] healthArray = { 1, 2, 2, 3 };
+    private float[] speedArray = { 0.5f, 0.5f, 1f, 0.5f };
+    private int[] scoreArray = { 100, 200, 300, 400 };
+
+    public int Type
+    {
+        set
+        {
+            health = healthArray[value - 1];
+            speed = speedArray[value - 1];
+            score = scoreArray[value - 1];
+        }
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        m_PlayerNumber = -1;
+        moveDirection = Vector2.down;
+        m_ChargeTime = 2f;
+        directionChangeTimer = 0;
+        directionChangeInteval = Random.Range(3, 5);
+    }
+
+    public IEnumerator Born(int type, int position)
+    {
+        Type = type;
+        animator.SetInteger("type", type);
+        animator.SetInteger("health", health);
+
+        Vector2[] enemySpawnPoint = { new Vector2(-3f, 3f), new Vector2(0f, 3f), new Vector2(3f, 3f) };
+        transform.position = enemySpawnPoint[position - 1];
+        float speed0 = speed;
+        speed = 0;
+        isInvincible = true;
+        yield return new WaitForSeconds(1f);
+        isInvincible = false;
+        speed = speed0;
+    }
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        m_CurrentChargeTime += Time.deltaTime;
+        if (m_CurrentChargeTime >= m_ChargeTime)
+        {
+            Fire();
+            m_CurrentChargeTime = 0f;
+        }
+        rigidbody2d.position += moveDirection * speed * Time.deltaTime;
+        directionChangeTimer += Time.deltaTime;
+        if (directionChangeTimer > directionChangeInteval)
+        {
+            SelectDirection();
+            directionChangeInteval = Random.Range(1, 3);
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.name == "WorldLimiter")
+        {
+            Debug.Log("tank hit ex bound");
+            SelectDirection();
+        }
+    }
+
+    private void SelectDirection()
+    {
+        float[] directChance = { 0.1f, 0.45f, 0.2f, 0.2f };
+        if (transform.position.y <= -3f)
+            directChance[1] = 0;
+        else if (transform.position.y >= 3f)
+            directChance[0] = 0;
+        if (transform.position.x < 0)
+        {
+            directChance[2] = 0.15f;
+            directChance[3] = 0.3f;
+            if (transform.position.x <= -3f)
+                directChance[2] = 0f;
+        }
+        else if (transform.position.x > 0)
+        {
+            directChance[2] = 0.3f;
+            directChance[3] = 0.15f;
+            if (transform.position.x >= -3f)
+                directChance[3] = 0f;
+        }
+        Vector2[] directChoice = { Vector2.up, Vector2.down, Vector2.left, Vector2.right };
+        moveDirection = directChoice[Choose(directChance)];
+
+        if (moveDirection == Vector2.up)
+            rigidbody2d.rotation = 0f;
+        else if (moveDirection == Vector2.down)
+            rigidbody2d.rotation = 180f;
+        else if (moveDirection == Vector2.left)
+            rigidbody2d.rotation = 90f;
+        else if (moveDirection == Vector2.right)
+            rigidbody2d.rotation = -90f;
+
+        Vector2 position = rigidbody2d.position;
+        position.x = Mathf.RoundToInt(position.x / smallestGrid) * smallestGrid;
+        position.y = Mathf.RoundToInt(position.y / smallestGrid) * smallestGrid;
+        rigidbody2d.position = position;
+
+        directionChangeTimer = 0;
+
+    }
+
+    int Choose(float[] probs)
+    {
+
+        float total = 0;
+
+        foreach (float elem in probs)
+        {
+            total += elem;
+        }
+
+        float randomPoint = Random.value * total;
+
+        for (int i = 0; i < probs.Length; i++)
+        {
+            if (randomPoint < probs[i])
+            {
+                return i;
+            }
+            else
+            {
+                randomPoint -= probs[i];
+            }
+        }
+        return probs.Length - 1;
+    }
+
+}
